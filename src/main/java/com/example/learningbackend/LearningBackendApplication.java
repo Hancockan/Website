3c4dd6c0926e9d9020309a9a89029b3c258d112d
Andrew Hancock <AndrewHancock0@Gmail.com>
2024-12-27 10:46:14 -0600

    Messing around with a simple form.

---
+++ src/main/java/com/example/learningbackend/LearningBackendApplication.java
@@ -0,0 +1,14 @@
+package com.example.learningbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningBackendApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(LearningBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/learningbackend/LearningBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.learningbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
